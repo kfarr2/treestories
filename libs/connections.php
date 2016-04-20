da4e46607659ef86e4828a443039e962561e1c26
Konstantin Farrell <kfarr2@hrimfaxi.oit.pdx.edu>
2016-04-20 15:40:43 -0700

    Fix constant definitions

--- libs/connections.php
+++ libs/connections.php
@@ -6,12 +6,12 @@
  */
 
 // Define a ROOT filepath
-define("ROOT", realpath(__DIR__ . "/../"));
+define(ROOT, realpath(__DIR__ . "/../"));
 
 set_include_path(get_include_path() . PATH_SEPARATOR . ROOT);
 
 // Define the facebook directory (for the php API)
-define('FACEBOOK_SDK_V4_SRC_DIR', ROOT.'/libs/facebook-php-sdk-v4/src/Facebook/');
+define(FACEBOOK_SDK_V4_SRC_DIR, ROOT.'/libs/facebook-php-sdk-v4/src/Facebook/');
 
 if(!file_exists(ini_get('session.save_path'))){
     mkdir(ini_get('session.save_path'), 0777, true);
@@ -30,10 +30,11 @@ $username = $_config['db_user'];
 $password = $_config['db_password'];
 
 // Get Facebook API stuff
-define("FACEBOOK_APP_ID", $_config['fb_app_id']);
+define(FACEBOOK_APP_ID, $_config['fb_app_id']);
 
 $fb_app_id = FACEBOOK_APP_ID;
-define("FACEBOOK_APP_SECRET", $_config['fb_app_secret']);
+define(FACEBOOK_APP_SECRET, $_config['fb_app_secret']);
+define(FACEBOOK_ACCESS_TOKEN, $_config['fb_access_token']);
 $fb_app_secret = FACEBOOK_APP_SECRET;
 $fb_access_token = $_config['fb_access_token'];
 
