0393f4ce695a51c9a987073c1d8f5339a7c43350
Konstantin Farrell <kfarr2@hera.rc.pdx.edu>
2016-05-03 11:48:38 -0700

    Pass site name to splash screen through url

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    $title = $site_name;
     include("libs/connections.php");
+    $title = $site_name;
     include("libs/helpers.php");
     include("libs/main_header.php");
     if (!isset($_SESSION['CREATED'])) {
@@ -29,7 +29,7 @@
             <h1><?php echo $title; ?></h1>
         </div>
         <div id="main-text">
-            <iframe	src='https://climatecope.research.pdx.edu/csSS/csSS.php'></iframe>
+            <iframe	src='https://climatecope.research.pdx.edu/csSS/csSS.php?title=<?php echo $title; ?>'></iframe>
         </div>
         <div id="footer">
             <button id="tell-my-story" onclick="destroyButton()">Tell My Story</button>
