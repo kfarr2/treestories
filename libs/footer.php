cbe9f071ede250fe31255d07ac82111f97275727
Konstantin Farrell <kfarr2@hera.rc.pdx.edu>
2016-05-27 15:34:10 -0700

    Add call to popup.js in footer

--- libs/footer.php
+++ libs/footer.php
@@ -1,4 +1,5 @@
-<script type="text/javascript" src="<?php echo BASE_DIR; ?>js/main.js"></script>
+<script src="<?php echo BASE_DIR; ?>js/popup.js" type="text/javascript"></script>
+<script src="<?php echo BASE_DIR; ?>js/main.js" type="text/javascript"></script>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
 <script src="<?php echo BASE_DIR; ?>js/resumable.js" type="text/javascript"></script>
 </body>
