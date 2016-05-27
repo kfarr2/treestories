f563e826ec6c7359e33b499e13ff2a4e784cc512
Konstantin Farrell <kfarr2@hrimfaxi.oit.pdx.edu>
2016-05-27 16:13:41 -0700

    Handle posts/neighborhoods with only 1 or 2 images

--- js/popup-action.js
+++ js/popup-action.js
@@ -4,14 +4,19 @@ function animateImages(){
     setTimeout(function(){
         if($('#popup-image-container').is(":visible")){
             var count = $('.popup-image').length;
-            var push = $('.popup-images').width() - $('#popup-image-container').width();
-            $('.popup-images').animate({'left': push * -1}, delay * count, function(){
-                setTimeout(function(){
-                    $('.popup-images').animate({'left': 0}, delay * count, function(){
-                        animateImages();
-                    });
-                }, delay);
-            });
+            if(count > 2){
+                var push = $('.popup-images').width() - $('#popup-image-container').width();
+                $('.popup-images').animate({'left': push * -1}, delay * count, function(){
+                    setTimeout(function(){
+                        $('.popup-images').animate({'left': 0}, delay * count, function(){
+                            animateImages();
+                        });
+                    }, delay);
+                });
+            } else {
+                var push = (($('.popup-images').width()) - $('#popup-image-container').width())/-2;
+                $('.popup-images').css('left', push);
+            }
         } else {
             setTimeout(function(){ animateImages(); }, 100);
         }
--- js/popup.js
+++ js/popup.js
@@ -2,25 +2,18 @@
 function createScrollDiv(image_urls, located, base_dir){
     var count = image_urls.length;
     var html = "<div id='popup-image-container'>";
-    if(count > 1){
-        var width = count * 100;
-        html += "<div class='popup-images' style='width: "+width+"px;'>"
-        html += '<a id="story-link" href="/cs/list.php?location='+located+'">';
-        for(i = 0; i < count; i++){
-            var filepath = base_dir+image_urls[i];
-            var offset = i*100;
-            html += '<div class="item popup-image" id="popup-image-'+i+'" ';
-            html += 'style="background-image: url(\''+filepath+'\'); ';
-            html += 'left: '+offset+'px;"></div>';
-        }
-        html += '</a>';
-        html += "</div>";
-    } else {
-        var filepath = base_dir+image_urls[0];
-        html += '<a id="story-link" href="/cs/list.php?location='+located+'">';
-        html += '<div class="popup-image" style="background-image: url(\''+filepath+'\'); background-repeat: no-repeat; background-position: center;">';
-        html += '</div></a>';
+    var width = count * 100;
+    html += "<div class='popup-images' style='width: "+width+"px;'>"
+    html += '<a id="story-link" href="/cs/list.php?location='+located+'">';
+    for(i = 0; i < count; i++){
+        var filepath = base_dir+image_urls[i];
+        var offset = i*100;
+        html += '<div class="item popup-image" id="popup-image-'+i+'" ';
+        html += 'style="background-image: url(\''+filepath+'\'); ';
+        html += 'left: '+offset+'px;"></div>';
     }
+    html += '</a>';
+    html += "</div>";
     html += "</div><hr />";
     return html;
 }
